4edc725fdf9ae39691b7131711b2d38fad3407ff
kveck <dsc_kveck@yahoo.com>
2024-01-09 15:11:43 -0800

    Updated sample data record

--- Program.cs
+++ Program.cs
@@ -15,26 +15,52 @@ namespace MigrateTOUData
             {
                 using (var dbContext = new VssDbContext())
                 {
-                    dbContext.Add(new Organization
+                    var newOrg = dbContext.Add(new Organization
                     {
-                        Name = "Test1",
-                        OrgCode = "Test01",
-                        Url = "Test",
-                        Email = "test@testemail.com",
-                        Phone = "5035551111"
+                        Name = "HomeStart",
+                        OrgCode = "HOMES",
+                        Url = @"https://www.homestart.org",
+                        Email = "",
+                        Phone = "6175420338",
+                        Fax = "6175421454",
+                        NextResourceId = 2,
+                        OrganizationAddresses = new List<OrganizationAddress>
+                        {
+                            new OrganizationAddress
+                            {
+                                Street1 = "105 Chauncy St",
+                                Street2 = "Suite 502",
+                                State = "MA",
+                                Zip = "02111",
+                                ZipExt = "1767",
+                                Country = "United States"
+                            }
+                        },
+                        ResourcePrograms = new List<ResourceProgram>
+                        { 
+                            new ResourceProgram
+                            {
+                                ResourceId = "HOMES1",
+                                Name = "Boston Youth Flex Fund",
+                                ResourceUrl = @"https://drive.google.com/file/d/1qYOKVRt0AgFz667V4_2HC-eDhTGAjC_k/view",
+                                Description = "Provides one-time payment to youth experiencing housing instability.",
+                                Cost = "FREE",
+                                Notes = "Applications must be completed by a case manager (no self-referrals).",
+                                Status = "VERIFIED"
+                            }
+                        }
                     });
 
                     try
                     {
                         Console.WriteLine("Before Save Changes");
                         dbContext.SaveChanges();
-                        Console.WriteLine("After Save Changes");
+                        Console.WriteLine($"After Save Changes");
                     }
-                    finally
+                    catch (Exception ex)
                     {
-
+                        Console.WriteLine($"save failed; msg = {ex}");
                     }
-
                 }
             }
             catch (Exception ex) { 
